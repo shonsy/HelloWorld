e1cd3e4c4e7e3aeebfb0f0c59fcf54d6fadb4876
shonsy <shonsy1999@gmail.com>
2017-07-12 10:26:29 +0800

    test

---
+++ factory/pizzas/Snippet.java
@@ -0,0 +1,12 @@
+package headfirst.factory.pizzas;
+
+public class Snippet {
+	 具有Java应用开发经验，能独立承担设计及开发任务；
+	2、 熟练掌握Java/J2EE相关技术，对程序设计有着清晰认识；
+	3、 深入了解Java流行开源项目(Eclipse、Spring等)，具有源代码阅读及分析能力；
+	4、熟悉基本的Web 应用开发技术，如HTML/XHTML,AJAX,JavaScript, J2EE,JSP,Servlet,Web Service,XML等； 
+	5、 有一定的数据库基础，会使用任何主流数据库（如ORACLE、DB2、SQLSERVER）进行开发，精通SQL语法； 
+	2	银行软件工程师/C语言方向
+	
+}
+
